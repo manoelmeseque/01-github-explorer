b0259b3100c61427c2295bc0bdafc18f565f8a76
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2022-09-01 22:58:24 -0300

    Configure webpack dev server

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export function App(){
     return(
-        <h1>Hello World</h1>
+        <div>
+            <h1>Hello World</h1>
+            <p>Bem vindo ao ignite 2022</p>
+        </div>
     )
 }

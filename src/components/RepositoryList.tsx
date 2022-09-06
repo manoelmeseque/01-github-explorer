7552bbb86ff76bb87dcc118d7ca8387611d63734
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2022-09-05 22:15:21 -0300

    Adicionando tipagem para os componentes

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -5,16 +5,15 @@ import '../styles/repositories.scss';
 
 // https://api.github.com/orgs/rocketseat/repos
 
-
-const repository = {
-    name: 'Repositorio 1',
-    description: 'Forms in ReactJS',
-    link: 'https://github.com/unform/unform'
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
 }
 
 export function RepositoryList(){
 
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -28,7 +27,10 @@ export function RepositoryList(){
 
             <ul>
                 {repositories.map(repository => {
-                    return <RepositoryItem key={repository.name} repository={repository} />
+                    return <RepositoryItem 
+                    key={repository.name} 
+                    repository={repository} 
+                    />
                 })}
             </ul>
         </section>
